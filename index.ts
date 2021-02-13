e9624dc39790ff919f30175e2f80c2ed8899bd6d
Creo 15 <creo15@MacBook-Air-de-Creo.local>
2021-02-12 23:02:04 -0600

    mi cambio Cristopher

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ function MostrarContenido(){
     let num1:number=0
     let num2:number=0
     let operacion:number=num1+num2
-    alert('HOLA A TODOS')  
+    alert('HOLA es un cambio por Cristopher')  
     alert('El Resultado es:' + operacion)
 }
     
