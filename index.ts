00e48f02774ce594dd88354abb7ee9c639972664
Pablo Ramirez <jpramirezp@hotmail.com>
2021-02-12 22:51:38 -0600

    Cambios de prueba

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 
 function MostrarContenido(){
+
+    let num1:number=0
+    let num2:number=0
+    let operacion:number=num1+num2
     alert('HOLA A TODOS')  
+    alert('El Resultado es:' + operacion)
 }
     
 
